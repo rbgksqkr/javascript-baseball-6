db25a1c9868d515b9098e8113b45032cbad2e7f8
rbgksqkr <rbgks1937@naver.com>
2023-12-14 09:05:03 +0900

    feat: 컴퓨터 서로 다른 숫자 뽑기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    const computer = this.getComputerNumber();
+    console.log(computer);
+    const user = await MissionUtils.Console.readLineAsync('');
+    console.log(user);
+  }
+  getComputerNumber() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    console.log(computer);
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
