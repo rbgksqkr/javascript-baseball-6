c85248a86baec30572e63f8a8c00c490457e6840
rbgksqkr <rbgks1937@naver.com>
2023-12-14 13:05:25 +0900

    test: 예외 처리 및 도메인 로직에 대한 테스트 코드 작성

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -44,7 +44,109 @@ describe('숫자 야구 게임', () => {
     });
   });
 
-  test('예외 테스트', async () => {
+  test('1스트라이크 테스트', async () => {
+    // given
+    const randoms = [1, 3, 5];
+    const answers = ['124', '135'];
+    const logSpy = getLogSpy();
+    const messages = ['1스트라이크'];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    // when
+    const app = new App();
+    await expect(app.play()).resolves.not.toThrow();
+
+    // then
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test('2스트라이크 테스트', async () => {
+    // given
+    const randoms = [1, 3, 5];
+    const answers = ['134', '135'];
+    const logSpy = getLogSpy();
+    const messages = ['2스트라이크'];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    // when
+    const app = new App();
+    await expect(app.play()).resolves.not.toThrow();
+
+    // then
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test('2볼 1스트라이크 테스트', async () => {
+    // given
+    const randoms = [1, 3, 5];
+    const answers = ['315', '135'];
+    const logSpy = getLogSpy();
+    const messages = ['2볼 1스트라이크'];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    // when
+    const app = new App();
+    await expect(app.play()).resolves.not.toThrow();
+
+    // then
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test('1부터 9까지의 숫자가 아니면 예외가 발생한다.', async () => {
+    // given
+    const randoms = [1, 3, 5];
+    const answers = ['120'];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    // when & then
+    const app = new App();
+
+    await expect(app.play()).rejects.toThrow('[ERROR]');
+  });
+
+  test('숫자가 아니면 예외가 발생한다.', async () => {
+    // given
+    const randoms = [1, 3, 5];
+    const answers = ['12a'];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    // when & then
+    const app = new App();
+
+    await expect(app.play()).rejects.toThrow('[ERROR]');
+  });
+
+  test('서로 다른 숫자가 아니면 예외가 발생한다.', async () => {
+    // given
+    const randoms = [1, 3, 5];
+    const answers = ['112'];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    // when & then
+    const app = new App();
+
+    await expect(app.play()).rejects.toThrow('[ERROR]');
+  });
+
+  test('숫자가 3자리가 아니면 예외가 발생한다.', async () => {
     // given
     const randoms = [1, 3, 5];
     const answers = ['1234'];
