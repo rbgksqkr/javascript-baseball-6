fa442b3c2183a2b46c9fee37422536606493c7ec
rbgksqkr <rbgks1937@naver.com>
2023-12-14 09:33:33 +0900

    feat: 스트라이크, 볼, 낫싱 판단

--- src/App.js
+++ src/App.js
@@ -2,9 +2,35 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 
 class App {
   async play() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
     const computer = this.getComputerNumber();
-    const user = await MissionUtils.Console.readLineAsync('');
-    console.log(this.getUserNumberList(user));
+    const userInput = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
+    const user = this.getUserNumberList(userInput);
+    this.getBaseballResult(computer, user);
+  }
+
+  getBaseballResult(computer, user) {
+    let strike = 0;
+    let ball = 0;
+    for (let i = 0; i < computer.length; i++) {
+      if (user[i] === computer[i]) {
+        strike += 1;
+        break;
+      }
+      if (computer.includes(user[i])) {
+        ball += 1;
+      }
+    }
+    if (ball === 0 && strike === 0) {
+      MissionUtils.Console.print(`낫싱`);
+      return 0;
+    } else if (ball > 0 && strike === 0) {
+      MissionUtils.Console.print(`${ball}볼`);
+    } else if (strike > 0 && ball === 0) {
+      MissionUtils.Console.print(`${strike}스트라이크`);
+    } else if (ball > 0 && strike > 0) {
+      MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
+    }
   }
 
   getUserNumberList(numbers) {
@@ -19,7 +45,7 @@ class App {
         computer.push(number);
       }
     }
-    console.log(computer);
+    return computer;
   }
 }
 
