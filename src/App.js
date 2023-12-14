052a4e2cb361e0486c0935ec10d0a3ad648e2770
rbgksqkr <rbgks1937@naver.com>
2023-12-14 09:39:49 +0900

    feat: 3스트라이크 판단 후 게임 완전히 종료

--- src/App.js
+++ src/App.js
@@ -4,18 +4,29 @@ class App {
   async play() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
     const computer = this.getComputerNumber();
+    console.log(computer);
     const userInput = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
     const user = this.getUserNumberList(userInput);
-    this.getBaseballResult(computer, user);
+    const result = this.getBaseballResult(computer, user);
+    if (result === 1) {
+      MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+      const answer = MissionUtils.Console.readLineAsync(
+        '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n'
+      );
+      if (answer === 2) {
+        return 0;
+      }
+    }
   }
 
   getBaseballResult(computer, user) {
+    console.log(computer, user);
     let strike = 0;
     let ball = 0;
     for (let i = 0; i < computer.length; i++) {
       if (user[i] === computer[i]) {
         strike += 1;
-        break;
+        continue;
       }
       if (computer.includes(user[i])) {
         ball += 1;
@@ -24,6 +35,9 @@ class App {
     if (ball === 0 && strike === 0) {
       MissionUtils.Console.print(`낫싱`);
       return 0;
+    } else if (strike === 3) {
+      MissionUtils.Console.print(`${strike}스트라이크`);
+      return 1;
     } else if (ball > 0 && strike === 0) {
       MissionUtils.Console.print(`${ball}볼`);
     } else if (strike > 0 && ball === 0) {
